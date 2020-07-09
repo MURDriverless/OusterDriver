71a307f16790623a9ff70c71b18c3ca94c278165
Andrew Huang <huanga2@student.unimelb.edu.au>
2020-07-09 19:25:39 +1000

    implemented IMU Port listening

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 #include <cassert>
 #include <cstring>
 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+const static int lidarPort = 7502;
+const static int imuPort = 7503;
+
 int test();
 int test2(OS1::LidarDataBlock& datablock);
 int imuTest();
@@ -14,6 +22,50 @@ int main(int argc, char** argv) {
     test();
     float_test();
     imuTest();
+
+    int sockfd;
+    uint8_t buffer[48];
+
+    struct sockaddr_in servaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    memset(&servaddr, 0, sizeof(servaddr));
+       
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(imuPort);
+
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    }
+
+   
+    while (true) {
+        recvfrom(sockfd, buffer, 48,  
+                MSG_WAITALL, nullptr, nullptr);
+
+        OS1::ImuPacket imuPacket = OS1::ImuPacket(buffer);
+
+        std::cout << std::dec << std::endl;
+        std::cout << imuPacket.diagnostic_time << std::endl;
+        std::cout << imuPacket.accelerometer_time << std::endl;
+        std::cout << imuPacket.gyroscope_time << std::endl;
+        std::cout << std::endl;
+        std::cout << imuPacket.x_accel << std::endl;
+        std::cout << imuPacket.y_accel << std::endl;
+        std::cout << imuPacket.z_accel << std::endl;
+        std::cout << std::endl;
+        std::cout << imuPacket.x_rot << std::endl;
+        std::cout << imuPacket.y_rot << std::endl;
+        std::cout << imuPacket.z_rot << std::endl;
+        std::cout << std::endl;
+    }
 
     return 0;
 }
